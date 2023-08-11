5d77972d63f54f4474632fa8babf617096fca440
navneet-chugh <navneetchugh15@gmail.com>
2023-08-11 20:16:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-//your code here
 let bandNames = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+
 function removeArticles(name) {
-	return name.replace(/^(a |an |the )/i, '');
-        }
+    return name.replace(/^(a |an |the )/i, '');
+}
+
 bandNames.sort((a, b) => removeArticles(a).localeCompare(removeArticles(b)));
 
-        const bandList = document.getElementById('band');
-        for (const bandName of bandNames) {
-            const li = document.createElement('li');
-            li.textContent = bandName;
-            bandList.appendChild(li);
-        }
+const bandList = document.getElementById('band');
+for (const bandName of bandNames) {
+    const li = document.createElement('li');
+    li.textContent = bandName;
+    bandList.appendChild(li);
 }
